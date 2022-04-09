a6d2a6088b44b4d377d926f2772de0453c84a54a
musekorea <musekorea@hanmail.net>
2022-04-09 16:24:21 +0800

    apollo client with React

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App></App>);
+root.render(
+	<ApolloProvider client={client}>
+		<App></App>
+	</ApolloProvider>
+);
