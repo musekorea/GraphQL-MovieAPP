3fe6b4651245fc90a0d296b50364ae364f468ccb
musekorea <musekorea@hanmail.net>
2022-04-10 14:54:50 +0800

    useQuery

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,30 @@
+import { gql, useQuery } from "@apollo/client";
+
+const graphQuery = gql`
+	query GetMovies {
+		movies {
+			id
+			title
+			medium_cover_image
+		}
+	}
+`;
+
 const Home = () => {
-	return (
-		<div>
-			<h1>HOME</h1>
-		</div>
-	);
+	const { loading, error, data } = useQuery(graphQuery);
+	console.log(data);
+	if (loading) {
+		return <h1>Loading</h1>;
+	}
+	if (data) {
+		return (
+			<div>
+				{data.movies.map((movie) => (
+					<img src={movie.medium_cover_image} key={movie.id} />
+				))}
+			</div>
+		);
+	}
 };
 
 export default Home;
